8030f1007d1a19a07a55e9c629453031dc4be527
Nikulin Dmitriy <dmin@ciklum.com>
2013-09-03 15:21:38 +0300

    Added style to Yandex metrika

--- nopCommerce/Plugins/Nop.Plugin.SMS.Verizon/OrderPlacedEventConsumer.cs
+++ nopCommerce/Plugins/Nop.Plugin.SMS.Verizon/OrderPlacedEventConsumer.cs
@@ -41,11 +41,11 @@ namespace Nop.Plugin.SMS.Verizon
             var order = eventMessage.Order;
 
             //send SMS
-            if (plugin.SendSms(String.Format("New order(#{0}) has been placed.", order.Id)))
+			if (plugin.SendSms(String.Format("Dear store owner! New order(#{0}) has been placed.", order.Id)))
             {
                 order.OrderNotes.Add(new OrderNote()
                 {
-                    Note = "\"Order placed\" SMS alert (to store owner) has been sent",
+					Note = "\"Dear store owner! Order placed\" SMS alert (to store owner) has been sent",
                     DisplayToCustomer = false,
                     CreatedOnUtc = DateTime.UtcNow
                 });
