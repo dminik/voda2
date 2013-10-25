dd257cfe404e009b18607c84287c8a46954b69f0
Nikulin Dmitriy <dmin@ciklum.com>
2013-10-25 17:31:35 +0300

    Added controller and view for parser category

---
+++ nopCommerce/Presentation/Nop.Web/Administration/Controllers/YandexMarketParserCategoryController.cs
@@ -0,0 +1,49 @@
+﻿namespace Nop.Admin.Controllers
+{
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Web.Mvc;
+
+	using Nop.Admin.Models.YandexMarket;
+	using Nop.Core;
+	using Nop.Core.Domain.YandexMarket;
+	using Nop.Services.Catalog;
+	using Nop.Services.YandexMarket;
+	using Nop.Web.Framework;
+	using Nop.Web.Framework.Controllers;
+
+	using Telerik.Web.Mvc;
+
+	[AdminAuthorize]
+	public class YandexMarketParserCategoryController : Controller
+	{
+		private readonly IYandexMarketCategoryService _yandexMarketCategoryService;
+		private readonly ICategoryService _shopCategoryService;
+		private readonly IProductService _productService;
+		 private readonly IStoreContext _storeContext;
+
+		 public YandexMarketParserCategoryController(
+			IYandexMarketCategoryService yandexMarketCategoryService, 
+			ICategoryService shopCategoryService,
+			IProductService productService,
+			IStoreContext storeContext)
+		{
+			this._yandexMarketCategoryService = yandexMarketCategoryService;
+			_shopCategoryService = shopCategoryService;
+			this._productService = productService;
+			_storeContext = storeContext;
+		}
+
+		 public ActionResult YandexMarketParserCategory()
+		 {			 
+			 return View();
+		 }
+
+		[HttpPost]
+		public ActionResult CategoryParseAndShow(string urlCategoryForParsing)
+		{
+			//_yandexMarketCategoryService.SetActiveAllParserCategoties(isActive);
+			return Json(new { Hehe="NeHehe" });
+		}
+	}
+}
