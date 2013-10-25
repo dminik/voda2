237a4013cda6437cae032e7f6cee08b1c448a040
Nikulin Dmitriy <dmin@ciklum.com>
2013-10-26 01:35:01 +0300

    Parsed only needed category

--- nopCommerce/Libraries/Nop.Services/SiteParsers/Categories/BaseParserCategories.cs
+++ nopCommerce/Libraries/Nop.Services/SiteParsers/Categories/BaseParserCategories.cs
@@ -2,28 +2,15 @@ namespace Nop.Services.SiteParsers.Categories
 {
 	using System;
 	using System.Collections.Generic;
-	using System.Linq;
-	using System.Text.RegularExpressions;
-	using System.Threading;
-
-	using Newtonsoft.Json;
-
 	using Nop.Core.Domain.YandexMarket;
 	using Nop.Services.Logging;
 	using Nop.Services.YandexMarket;
-
 	using OpenQA.Selenium;
 	using OpenQA.Selenium.IE;
-	using OpenQA.Selenium.Remote;
 
-	class ExistedProduct
-	{
-		public string Url { get; set; }
-		public bool IsFantom { get; set; }
-	}
 
 	public abstract class BaseParserCategories
-	{
+	{		
 		public void Init(string urlCategoryForParsing, ILogger logger, IYandexMarketCategoryService yandexMarketCategoryService)
 		{			
 			this.mLogger = logger;
@@ -33,77 +20,25 @@ namespace Nop.Services.SiteParsers.Categories
 		}
 
 		private IYandexMarketCategoryService _yandexMarketCategoryService;		
-		private string UrlCategoryForParsing { get; set; }
+		protected string UrlCategoryForParsing { get; set; }
 		
 		protected ILogger mLogger;
 		protected IWebDriver mDriver;
 
-		protected virtual string CssSelectorForProductLinkInProductList { get{throw new Exception("Not implemented");}}				
-		protected virtual string CssSelectorForProductArticulInProductPage { get { return ""; } }
-		
-		protected virtual string GetLinkToSpecsTabOfProduct(string productLink)
+		// protected virtual string CssSelectorForProductLinkInProductList { get{throw new Exception("Not implemented");}}				
+		// protected virtual string CssSelectorForProductArticulInProductPage { get { return ""; } }
+
+		protected virtual YandexMarketCategoryRecord GetParserCategory()
 		{
-			return productLink;
+			throw new Exception("Not implemented");
 		}
 		
 
-		class XXX
-		{
-		//	"id":"181",
-		//"level":0,
-		//"pid":"0",
-		//"expanded":false,
-		//"have_children":true,
-		//"name":"\u041a\u043e\u043c\u043f\u044c\u044e\u0442\u0435\u0440\u043d\u0430\u044f \u0442\u0435\u0445\u043d\u0438\u043a\u0430",
-		//"active":false,
-		//"is_first":false,
-		//"show":true,
-		//"url":"kompyuternaya-tehnika-portal"
-
-			public string id;
-			public int level;
-			public string pid;
-			public bool expanded;
-			public bool have_children;
-			public string name;
-			public bool active;
-			public bool is_first;
-			public bool show;
-			public string url;
-
-		}
-
-		private YandexMarketCategoryRecord ProcessCategory(XXX category, XXX[] categoriesArray)
-		{
-			var newCategory = new YandexMarketCategoryRecord()
-			{
-				IsActive = true,
-				Name = category.name,
-				ParentId = int.Parse(category.pid),
-				Url = category.url,
-				Id = int.Parse(category.id),
-				Children = new List<YandexMarketCategoryRecord>(),
-			};
-
-			if (category.have_children)
-			{
-				var children = categoriesArray.Where(x => x.pid == category.id);
-				foreach (var child in children)
-				{
-					var newChild = ProcessCategory(child, categoriesArray);
-					newCategory.Children.Add(newChild);
-				}
-			}
-				
-
-			return newCategory;
-		}
-
-		public List<YandexMarketCategoryRecord> Parse(ref bool isStopProducsImport)
+		public YandexMarketCategoryRecord Parse(ref bool isStopProducsImport)
 		{
 			this.mLogger.Debug("Start Parsing Category ...");
 
-			var resultCategoriesHierarchy = new List<YandexMarketCategoryRecord>();
+			YandexMarketCategoryRecord resultCategory = null;
 
 			try
 			{
@@ -112,80 +47,8 @@ namespace Nop.Services.SiteParsers.Categories
 
 				// Ссылка на список товаров
 				this.mDriver.Navigate().GoToUrl(this.UrlCategoryForParsing);
-				//Thread.Sleep(3000);
 
-				
-					// Найти все ссылки на товары	
-				//CssSelectorForProductLinkInProductList = ".cat-menu .level1 a.have-children em";
-				//var allcollapsedMenus = mDriver.FindElements(By.CssSelector(CssSelectorForProductLinkInProductList)));
-
-				string source = mDriver.PageSource;
-				var indexStart = source.IndexOf("var CATEGORIES = [", System.StringComparison.Ordinal) + "var CATEGORIES = [".Length - 1;
-				var indexEnd = source.IndexOf("}];", indexStart, System.StringComparison.Ordinal);
-
-				var categoriesText = source.Substring(indexStart, indexEnd - indexStart + 2);
-
-				var categoriesArray = JsonConvert.DeserializeObject<XXX[]>(categoriesText);
-
-				
-				// всем елементам меню верхнего уровня
-				foreach (var currentCategory in categoriesArray.Where(x => x.level == 0))
-				{
-					var category = ProcessCategory(currentCategory, categoriesArray);
-
-					resultCategoriesHierarchy.Add(category);
-				}
-
-				//foreach (var allcollapsedMenu in allcollapsedMenus)
-				//{
-				//	allcollapsedMenu.Click();
-				//}
-
-				//bool isNextPage = false;				
-				//int pageLinksCounter = 1;
-
-				//do
-				//{
-				//	if (isStopProducsImport)
-				//		throw new Exception("Stopped by user.");
-
-				//	// Найти все ссылки на товары			
-				//	var linksFromCurrentPage =
-				//		Enumerable.ToList<string>(
-				//			this.mDriver.FindElements(By.CssSelector(CssSelectorForProductLinkInProductList)).Select(s => s.GetAttribute("href")));
-					
-				//	this.mLogger.Debug("Have page " + pageLinksCounter++ + " with links");
-
-				//	var nextPageUrl = "";
-				//	try
-				//	{
-				//		//Ищем ссылку на следущую страницу, если она есть
-				//		if (!this.mDriver.PageSource.Contains(NextLinkInProductListName)) 
-				//			throw new Exception();
-
-				//		nextPageUrl = this.mDriver.FindElement(By.CssSelector(CssSelectorForNextLinkInProductList)).GetAttribute("href");
-				//		nextPageUrl = nextPageUrl.Replace("pg", "/pg").Replace("//pg", "/pg");
-
-				//		isNextPage = true;						
-				//	}
-				//	catch (Exception)
-				//	{
-				//		isNextPage = false;
-				//	}
-
-				//	// ===== Парсим все товары с страницы
-				//	resultProductList.AddRange(GetProductsByLinks(linksFromCurrentPage));
-
-				//	if (this.ParseNotMoreThen <= resultProductList.Count) break;
-
-
-				//	if (isNextPage) // Жммем на следущую страницу, если она есть
-				//	{ 																	
-				//		this.mDriver.Navigate().GoToUrl(nextPageUrl);
-				//		//Thread.Sleep(3000);						
-				//	}
-				//}
-				//while (isNextPage);				
+				resultCategory = GetParserCategory();
 			}
 			catch (Exception ex)
 			{
@@ -199,59 +62,9 @@ namespace Nop.Services.SiteParsers.Categories
 
 			this.mLogger.Debug("End Parsing.");
 
-			return resultCategoriesHierarchy;
+			return resultCategory;
 		}
-
 		
-
-		//private YandexMarketCategoryRecord CreateProduct(string productLink)
-		//{			
-		//	var product = new YandexMarketCategoryRecord();
-		//	product.Url = productLink;
-
-		//	// Переходим на страницу спецификации товара
-		//	var pageSpecsUrl = this.GetLinkToSpecsTabOfProduct(productLink);
-		//	this.mDriver.Navigate().GoToUrl(pageSpecsUrl);
-		//	//Thread.Sleep(3000);
-
-		//	this.mLogger.Debug("Have specs page " + pageSpecsUrl);
-
-		//	product.YandexMarketCategoryRecordId = this.ParserCategoryId;
-
-		//	// Artikul
-		//	if (this.CssSelectorForProductArticulInProductPage != string.Empty)
-		//		product.Articul = this.FindElement(this.CssSelectorForProductArticulInProductPage).Text.Replace("Код: ", "");
-
-		//		if (CssSelectorForProductFullDescriptionInProductPage != string.Empty)
-		//		{
-		//			try
-		//			{
-		//				product.FullDescription =
-		//					this.GetInnerHtml(this.mDriver.FindElement(By.CssSelector(CssSelectorForProductFullDescriptionInProductPage)));
-		//			}
-		//			catch (Exception ex)
-		//			{
-		//				product.FullDescription = "Нет описания";
-
-		//				if (!ex.Message.Contains("Unable to find element")) 
-		//					throw;
-		//			}
-		//		}
-
-		//		product = ProductPostProcessing(product);				
-		//	}
-			
-
-		//	this.mLogger.Debug("Saving product...");
-		//	this._yandexMarketCategoryService.Insert(product);
-		//	this.mLogger.Debug("Saving product Done.");
-
-		//	if(!product.IsNotInPriceList)
-		//		return product;
-		//	else			
-		//		return null;			
-		//}
-
 		public static BaseParserCategories Create(string productsPageUrl, ILogger logger, IYandexMarketCategoryService _yandexMarketCategoryService)
 		{
 			BaseParserCategories parser = null;
--- nopCommerce/Libraries/Nop.Services/SiteParsers/Categories/UgContractParserCategory.cs
+++ nopCommerce/Libraries/Nop.Services/SiteParsers/Categories/UgContractParserCategory.cs
@@ -1,7 +1,17 @@
 namespace Nop.Services.SiteParsers.Categories
 {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+
+	using Newtonsoft.Json;
+
+	using Nop.Core.Domain.YandexMarket;
+
 	public class UgContractParserCategory : BaseParserCategories
 	{
+		YandexMarketCategoryRecord mNeededCategory = null;
+
 		// List
 		//protected override string CssSelectorForProductLinkInProductList { get { return ".goods-list:not(.goods-list-small) div.cat-item a"; } }
 		//protected override string CssSelectorForNextLinkInProductList { get { return "[title=следующая]"; } }
@@ -19,5 +29,73 @@ namespace Nop.Services.SiteParsers.Categories
 		//protected override string CssSelectorForProductSpecsRowsInProductPage { get { return ".properties-table tr"; } }
 		//protected override string CssSelectorForProductSpecKeyInProductPage { get { return "td.prop"; } }
 		//protected override string CssSelectorForProductSpecValInProductPage { get { return "td.val"; } }		
+
+		class JsonCategory
+		{
+			public string id;
+			public int level;
+			public string pid;
+			public bool expanded;
+			public bool have_children;
+			public string name;
+			public bool active;
+			public bool is_first;
+			public bool show;
+			public string url;
+
+		}
+
+		protected override YandexMarketCategoryRecord GetParserCategory()
+		{			
+			var resultCategoriesHierarchy = new List<YandexMarketCategoryRecord>();
+
+			string source = mDriver.PageSource;
+			var indexStart = source.IndexOf("var CATEGORIES = [", System.StringComparison.Ordinal) + "var CATEGORIES = [".Length - 1;
+			var indexEnd = source.IndexOf("}];", indexStart, System.StringComparison.Ordinal);
+
+			var categoriesText = source.Substring(indexStart, indexEnd - indexStart + 2);
+
+			var categoriesArray = JsonConvert.DeserializeObject<JsonCategory[]>(categoriesText);
+
+			// всем елементам меню верхнего уровня
+			foreach (var currentCategory in categoriesArray.Where(x => x.level == 0))
+			{
+				var category = ProcessCategory(currentCategory, categoriesArray);
+				resultCategoriesHierarchy.Add(category);
+
+				if (category.Url == base.UrlCategoryForParsing.Replace("yugcontract.ua/shop/", ""))
+					mNeededCategory = category;
+			}
+
+			return mNeededCategory;
+		}
+
+		private YandexMarketCategoryRecord ProcessCategory(JsonCategory category, JsonCategory[] categoriesArray)
+		{
+			var newCategory = new YandexMarketCategoryRecord()
+			{
+				IsActive = true,
+				Name = category.name,
+				ParentId = int.Parse(category.pid),
+				Url = category.url,
+				Id = int.Parse(category.id),
+				Children = new List<YandexMarketCategoryRecord>(),
+			};
+
+			if (newCategory.Url == base.UrlCategoryForParsing.Replace("yugcontract.ua/shop/", ""))
+				mNeededCategory = newCategory;
+
+			if (category.have_children)
+			{
+				var children = categoriesArray.Where(x => x.pid == category.id);
+				foreach (var child in children)
+				{
+					var newChild = ProcessCategory(child, categoriesArray);
+					newCategory.Children.Add(newChild);
+				}
+			}
+
+			return newCategory;
+		}
 	}
 }
